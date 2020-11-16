495713c649c0e38365c73b08901c05c8da61a483
SerenaBr <74198537+SerenaBr@users.noreply.github.com>
2020-11-16 10:57:19 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -1,3 +1,30 @@
 # R code for plotting the relationship among ecological variables
 
 install.packages('sp')
+library(sp)
+
+#data is use to recall dataset
+data(meuse)
+meuse
+# View->show the data in a different window
+View(meuse)
+
+head(meuse)
+
+#Exercise: mean of all of the variables?
+summary(meuse)
+
+#Exercise: plot zinc  (y) against cadmuim (x)
+#$->use to link different elements of the data
+plot(meuse$cadmium,meuse$zinc)
+
+attach(meuse)
+plot(cadmium,zinc)
+
+plot(cadmium,lead)
+
+#scatterplot matrices
+pairs (meuse)
+
+#Question: peiring only the elements part of the dataset: how to do that?
+#only with cadmium,copper,lead,zinc...
