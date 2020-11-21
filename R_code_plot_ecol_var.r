bd21463a90e9174ebb0e83cb625a85063c7eb4be
SerenaBr <74198537+SerenaBr@users.noreply.github.com>
2020-11-21 12:23:11 +0100

    Update R_code_plot_ecol_var.r

--- R_code_plot_ecol_var.r
+++ R_code_plot_ecol_var.r
@@ -28,3 +28,38 @@ pairs (meuse)
 
 #Question: peiring only the elements part of the dataset: how to do that?
 #only with cadmium,copper,lead,zinc...
+
+###Lecture n.2 on ecological variables###
+
+#exercise
+#Recall the package sp, recall the dataset meuse
+
+library(sp)
+data(meuse)
+
+pairs(meuse)
+
+head(meuse)
+
+#cadmium copper lead zinc
+#pairs with soil variables
+#from colomn 3 to colomn 6
+
+pairs(meuse[,3:6])
+
+#let's use the names of the columns
+pairs(~cadmium+copper+lead+zinc, data=meuse)
+
+#let's prettify the graph
+#Exercise: just use cadmium, lead and zinc
+pairs(~cadmium+lead+zinc, data=meuse)
+
+#Exercise: change the color
+pairs(~cadmium+copper+lead+zinc, data=meuse, col='red')
+
+#Exercise: change the symbol to filled triangles: pch
+pairs(~cadmium+copper+lead+zinc, data=meuse, col='red',pch=17)
+
+#exercise: increase the size of the triangles
+pairs(~cadmium+copper+lead+zinc, data=meuse, col='red',pch=17,cex=3)
+
