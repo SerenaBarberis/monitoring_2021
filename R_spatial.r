e6b56f3703d3ddd3e39aaddd7b6c7a0f413e88f9
SerenaBr <74198537+SerenaBr@users.noreply.github.com>
2020-11-22 22:19:01 +0100

    Update R_spatial.r

--- R_spatial.r
+++ R_spatial.r
@@ -27,3 +27,46 @@ bubble(meuse,'zinc')
 #do the same for lead
 bubble(meuse,'lead')
 bubble(meuse,'lead',col='red')
+
+### Installing the ggplot2 library/package
+
+install.packages('ggplot2')
+
+library(ggplot2)
+
+#ecological dataframe
+#biofuels
+biofuels<- c(120, 200, 350, 570, 750) #array of values: c
+#oxydative enzimes
+oxydative<- c(1200, 1300, 21000, 34000, 50000)
+
+#dataframe
+d<- data.frame(biofuels,oxydative)
+d
+
+#Ggplot function
+ggplot(d, aes (x= biofuels, y= oxydative))
+
+#geom_point function -> put the data in the graph
+ggplot(d, aes (x= biofuels, y= oxydative)) + geom_point()
+
+ggplot(d, aes (x= biofuels, y= oxydative)) + geom_point(col='red', size=5)
+
+# lines instead points
+ggplot(d, aes (x= biofuels, y= oxydative)) + geom_line()
+
+# lines and points on the graph
+ggplot(d, aes (x= biofuels, y= oxydative)) + geom_line() + geom_point(col='red', size=5)
+
+# polygons -> show the spread of the data
+ggplot(d, aes (x= biofuels, y= oxydative)) + geom_polygon()
+
+### IMPORT DATA FROM AN EXTERNAL SOURCE ###
+
+#setwd for windows
+setwd('C:/Users/Sery/Desktop/Lab')
+
+covid<- read.table('covid_agg.csv', header=TRUE)
+covid
+
+
