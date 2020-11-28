5cb93cf1486ed4d55f75c26aec79bab2d2848f63
SerenaBr <74198537+SerenaBr@users.noreply.github.com>
2020-11-28 23:17:52 +0100

    Update R_code_point_pattern_analysis.r

--- R_code_point_pattern_analysis.r
+++ R_code_point_pattern_analysis.r
@@ -81,6 +81,43 @@ points(covid_planar)
 plot(coastlines, add=T)  #the density is oriented to china because there are the high number of cases
 
 
+##### Plotting points with different size related to covid data together with the interpolated map
 
+library(spatstat)
 
+library (rgdal)
 
+covid <- read.table('covid_agg.csv', header=TRUE) # or read.csv
+
+head(covid)
+
+attach(covid)  #if you don't attach: covid$lon
+covid_planar <-ppp(lon, lat, c(-180,180), c(-90,90))
+
+marks(covid_planar) <- cases 
+cases_map <- Smooth(covid_planar)
+
+cl <- colorRampPalette(c('lightpink2', 'lightsalmon', 'tomato1', 'red3', 'maroon'))(100)
+
+### name of colour in R-> https://www.datanovia.com/en/blog/r-colors-amazong-resources-you-want-to-know/
+
+plot(cases_map, col=cl)
+
+###
+install.packages('sf')
+library(sf)
+
+Spoints <- st_as_sf(covid, coords = c('lon', 'lat'))  
+
+plot(Spoints, cex=Spoints$cases, col = 'purple3', lwd =3, add=T)  #lwd=line of the different points   #the points are out of map because represent the dimension of value
+
+plot(cases_map, col=cl)
+plot(Spoints, cex=Spoints$cases/10000, col = 'purple3', lwd =3, add=T)  #/10000 to reduce the dimension of the points
+
+# put the coastline on the map
+
+library(rgdal)
+coastlines <- readOGR('ne_10m_coastline.shp')
+plot(coastlines, add=T)
+
+#put a smoother to the coastlines by resampling
